e0020141e582d3231dbef0cf86d401eea8a51f9b
ArturoArielPerez <arturopereza997@gmail.com>
2020-09-01 14:04:32 -0300

    ProductController terminado

--- site/controller/productController.js
+++ site/controller/productController.js
@@ -12,20 +12,86 @@ module.exports ={
 
         res.render('productDetail', {product : producto[0]});
     },
-    vistaCart: function(req,res){
-        res.render('productCart');
-    },
     listarProductos:function(req,res){
         res.render('carta',{productos : productos});
     },
     agregar:function(req,res){
-        let product;
+    
+        res.render('productAdd', {
+        });
+    },
+    publicar: function(req,res,next){
+
+        let lasId = 1;
         
-        if(req.query.product){
-            producto = req.query.producto
+        productos.forEach(producto=>{
+            if(producto.id > lasId){
+                lasId = producto.id
+            }
+        })
+        
+        let newProduct ={
+            id: lasId+1,
+            nombre: req.body.nombre,
+            precio:Number(req.body.precio),
+            categoria: req.body.categoria,
+            descripcion: req.body.descripcion,
+            imagen: (req.files[0])?req.files[0].filename:"default-image.png"
         }
-        res.render('productAdd', {product : productos});
-}
+        productos.push(newProduct);
 
+        fs.writeFileSync(path.join(__dirname, '..', 'data', 'products.json'), JSON.stringify(productos), 'utf-8');
 
+        res.redirect('/products/carta');
+
+    },
+    eliminar : function(req,res) {
+        let id = req.params.id;
+        let producto;
+
+        productos.forEach(product => {
+            if(product.id == id){
+                producto = productos.indexOf(product);
+            }
+        });
+
+        productos.splice(producto,1);
+
+        fs.writeFileSync(path.join(__dirname, '../data/products.json'), JSON.stringify(productos))
+
+        res.redirect('/products/carta');
+    },
+    formularioEdit:function(req,res){
+        let id = req.params.id;
+        let resultado = productos.filter(producto=>{
+            return producto.id == id
+        });
+        res.render('productEdit', {
+            producto: resultado[0]
+        });
+    },
+    editar:function(req,res){
+        let id = req.params.id;
+        
+        productos.forEach(producto => {
+            if(producto.id == id){
+                producto.id = Number(id);
+                producto.nombre = req.body.nombre.trim();
+                producto.precio = Number(req.body.precio);
+                producto.categoria = req.body.categoria.trim();
+                producto.descripcion = req.body.descripcion.trim();
+                producto.imagen = (req.files[0]? req.files[0].filename : producto.imagen);
+
+            }
+        });
+        productosJSON = JSON.stringify(productos);
+
+        fs.writeFileSync(path.join(__dirname, '..', 'data', 'products.json') , productosJSON);
+
+        //res.redirect('products/'+id+'/edit');
+        res.redirect('/products/carta');
+    },
+    vistaCart: function(req,res){
+        res.render('productCart');
+         }
 }
