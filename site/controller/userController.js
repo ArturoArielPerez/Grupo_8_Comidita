8a23309eace8cf414ce7f06cfbfe702388c31079
juancruzjcm <juancruzm19@gmail.com>
2020-10-12 21:05:31 -0300

    bd actualizada

--- site/controller/userController.js
+++ site/controller/userController.js
@@ -1,52 +1,53 @@
 const usuarios = require('../data/users');
 const productos = require('../data/products');
 
-const {validationResult, body} = require('express-validator');
-const bcrypt =require('bcrypt');
+const db = require('../database/models')
+const { validationResult, body } = require('express-validator');
+const bcrypt = require('bcrypt');
 const fs = require('fs');
 const path = require('path');
 
 
 module.exports = {
     register: function(req, res) {
-        res.render('register',{
+        res.render('register', {
             title: 'Registrarse',
             css: 'register.css',
-            user:req.session.user
+            user: req.session.user
         });
     },
-    processRegister:function(req,res){
+    processRegister: function(req, res) {
         let errors = validationResult(req);
-        let lastID = 0;
-        if(usuarios.length > 0){
-            usuarios.forEach(usuario=>{
-                if(usuario.id > lastID){
-                    lastID = usuario.id
-                }
-            });
-        }
 
-        if(errors.isEmpty()){
-            let nuevoUsuario = {
-                id:lastID+1,
-                nombre:req.body.nombre,
-                apellido: req.body.apellido,
-                email:req.body.email,
-                avatar:(req.files[0])?req.files[0].filename:"default.png",
-                contraseña:bcrypt.hashSync(req.body.contraseña,10)
-            }
 
-            usuarios.push(nuevoUsuario);
+        if (errors.isEmpty()) {
 
-            fs.writeFileSync(path.join(__dirname,'..','data','users.json'),JSON.stringify(usuarios),'utf-8')
-            return res.redirect('/users/login')
-        }else{
-            res.render('register',{
-                title:"Registro de usuarios",
-                css:'register.css',
-                errors:errors.mapped(),
-                old:req.body,
-                user:req.session.user
+            db.Users.create({
+
+                nombre: req.body.nombre.trin(),
+                apellido: req.body.apellido.trin(),
+                email: req.body.email.trin(),
+                contraseña: bcrypt.hashSync(req.body.contraseña.trin(), 10),
+                avatar: (req.files[0]) ? req.files[0].filesname : 'default.png',
+                rol: 'user'
+            })
+
+            .then(result => {
+                    console.log(result);
+                    return res.redirect('/users/login')
+
+                })
+                .catch(errores => {
+                    console.log(errores);
+                })
+
+        } else {
+            res.render('register', {
+                title: "Registro de usuarios",
+                css: 'register.css',
+                errors: errors.mapped(),
+                old: req.body,
+                user: req.session.user
             });
         }
     },
@@ -54,57 +55,56 @@ module.exports = {
         res.render('login', {
             title: 'Login',
             css: 'login.css',
-            user:req.session.user
+            user: req.session.user
         });
     },
-    processLogin:function(req,res){
+    processLogin: function(req, res) {
         let errors = validationResult(req);
-        if(errors.isEmpty()){
-            usuarios.forEach(usuario=>{
-                if(usuario.email == req.body.email){
+        if (errors.isEmpty()) {
+            usuarios.forEach(usuario => {
+                if (usuario.email == req.body.email) {
                     req.session.user = {
-                        id:usuario.id,
-                        nick:usuario.nombre + ' ' + usuario.apellido,
-                        email:usuario.email,
-                        avatar:usuario.avatar,
-                        rol:usuario.rol
+                        id: usuario.id,
+                        nick: usuario.nombre + ' ' + usuario.apellido,
+                        email: usuario.email,
+                        avatar: usuario.avatar,
+                        rol: usuario.rol
                     }
                 }
             })
-            if(req.body.recordatorio){
-                res.cookie('Comidita',req.session.usuario,{maxAge:1000*60*2})
+            if (req.body.recordatorio) {
+                res.cookie('Comidita', req.session.usuario, { maxAge: 1000 * 60 * 2 })
             }
             return res.redirect('/');
-        }else{
-            return res.render('login',{
-                title:"Ingreso de Usuarios",
-                css:'login.css',
+        } else {
+            return res.render('login', {
+                title: "Ingreso de Usuarios",
+                css: 'login.css',
                 errors: errors.mapped(),
-                old:req.body,
-                user:req.session.user
+                old: req.body,
+                user: req.session.user
 
             })
         }
     },
-    logout:function(req,res){
+    logout: function(req, res) {
         req.session.destroy();
-        if(req.cookies.userComidita){
-            res.cookie('userComidita','',{maxAge:-1})
+        if (req.cookies.userComidita) {
+            res.cookie('userComidita', '', { maxAge: -1 })
         }
         res.redirect('/')
 
     },
-    profile:function(req,res){
+    profile: function(req, res) {
         res.render('profile', {
             title: 'Perfil de usuario',
             css: 'profile.css',
             user: req.session.user,
-            productos: productos.filter(producto=>{
+            productos: productos.filter(producto => {
                 return producto.categoria != "visited" && producto.categoria != "in-sale"
             })
         })
 
-}
+    }
 
 }
-
--- site/validators/registerValidator.js
+++ site/validators/registerValidator.js
@@ -1,19 +1,21 @@
-const usuarios = require('../data/users');
+/* const usuarios = require('../data/users'); */
 
-const {check,validationResult,body} = require('express-validator');
+const db = require('../database/models')
+
+const { check, validationResult, body } = require('express-validator');
 
 
 module.exports = [
 
     check('nombre')
     .isLength({
-        min:1
+        min: 1
     })
     .withMessage('Debes ingresar tu nombre'),
 
     check('apellido')
     .isLength({
-        min:1
+        min: 1
     })
     .withMessage('Debes ingresar tu apellido'),
 
@@ -22,26 +24,38 @@ module.exports = [
     .withMessage('Debes ingresar un email válido'),
 
     body('email')
-    .custom(function(value){
-        for(let i = 0; i<usuarios.length;i++){
-            if(usuarios[i].email == value){
+    .custom(function(value) {
+        /* for (let i = 0; i < usuarios.length; i++) {
+            if (usuarios[i].email == value) {
                 return false
             }
         }
-        return true
-    })
-    .withMessage('Este mail ya está registrado'),
+        return true */
+
+        return db.Users.findOne({
+                where: {
+
+                    email: value
+                }
+            })
+            .then(user => {
+                if (user) {
+                    return Promise.reject('Este mail ya esta registrado ')
+                }
+            })
+    }),
+    /* .withMessage('Este mail ya está registrado'), */
 
     check('contraseña')
     .isLength({
-        min:6,
-        max:12
+        min: 6,
+        max: 12
     })
     .withMessage('La contraseña debe tener entre 6 y 12 caracteres'),
 
     body('contraseña2')
-    .custom(function(value,{req}){
-        if(value != req.body.contraseña){
+    .custom(function(value, { req }) {
+        if (value != req.body.contraseña) {
             return false
         }
         return true
